37cdf5c6e9fa33677ab850a200a1ec4c8bafa29f
Tim Sievers <tim.willi.sievers@senvion.com>
2016-10-13 12:16:59 +0200

    the script is able to scan urls now; anyway on windows it still doesn't work correctly

--- diff_restricted_pages.js
+++ diff_restricted_pages.js
@@ -3,72 +3,70 @@
 "use strict";
 var RenderUrlsToFile, confFile, config, login, system;
 
-system = require("system");
-
-login = function(config, cb)
+system             = require("system");
+var CATCH_FUNCTION = function (err)
 {
-    var page = require('webpage').create(),
-        data = 'username=' + config.login.credentials.username + '&password=' + config.login.credentials.password;
+    for (var key in err)
+    {
+        console.log('CATCH!!! key: ' + key + ' => value: ' + err[key]);
+    }
+};
+phantom.onError    = CATCH_FUNCTION;
 
-    page.settings.userAgent = config.userAgent;
+var createPage = function()
+{
+    var page = require('webpage').create();
 
-    page.open(config.login.url, 'post', data, function (status) {
-        if (status !== 'success') {
-            cb('Unable to post!', null);
-        } else {
-            cb(null, page);
-        }
-        phantom.exit();
-    });
+    page.settings.userAgent       = config.userAgent;
+    page.settings.resourceTimeout = 2000;
+    page.onError                  = phantom.onError;
+    page.onResourceTimeout        = CATCH_FUNCTION;
+
+    return page;
 };
 
-
-if (system.args.length > 1) {
-    confFile = Array.prototype.slice.call(system.args, 1);
-} else {
-    console.log("Usage: phantomjs diff_restricted_pages.js [confFile]");
-    confFile = "./conf.d/default.json";
-}
-
-config = require(confFile);
-
-login(config, function(err, page)
+login = function (config, cb)
 {
-    if (err)
-    {
-        console.log(err);
-    }
-    else
-    {
-        console.log(page.content);
+    var page = createPage(),
+        data = '';
 
-        var cookies = page.cookies;
+    page.viewportSize = {
+        width : 1024,
+        height: 800
+    };
 
-        console.log('Listing cookies:');
-        for(var i in cookies) {
-            console.log(cookies[i].name + '=' + cookies[i].value);
+    for (var key in config.login.credentials)
+    {
+        if (data.length > 0)
+        {
+            data += "&";
         }
+        data += key + "=" + config.login.credentials[key];
     }
-});
 
-// Login durchführen (Cookie speichern)
-// Seiten rendern und in einem TEMP-Ordner ablegen
-// Bilder mit erwarteten Ergebnissen vergleichen
-    // Unterschiede sammeln
-    // Mail vorbereiten
-    // Mail versenden
+    try
+    {
+        page.open(config.login.url, 'post', data, function (status)
+        {
+            if (status !== 'success')
+            {
+                cb('Unable to post!', null);
+            }
+            else
+            {
+                page.render('./current/login.png');
 
-/*
-RenderUrlsToFile(confFile, (function(status, url, file) {
-    if (status !== "success") {
-        return console.log("Unable to render '" + url + "'");
-    } else {
-        return console.log("Rendered '" + url + "' at '" + file + "'");
+                page.close();
+                cb();
+            }
+            phantom.exit();
+        });
     }
-}), function() {
-    return phantom.exit();
-});
-*/
+    catch (err)
+    {
+        CATCH_FUNCTION(err);
+    }
+};
 
 /*
  Render given urls
@@ -76,45 +74,137 @@ RenderUrlsToFile(confFile, (function(status, url, file) {
  @param callbackPerUrl Function called after finishing each URL, including the last URL
  @param callbackFinal Function called after finishing everything
  */
-RenderUrlsToFile = function(urls, callbackPerUrl, callbackFinal) {
-    var getFilename, next, page, retrieve, urlIndex, webpage;
-    urlIndex = 0;
-    webpage = require("webpage");
-    page = null;
-    getFilename = function() {
-        return "rendermulti-" + urlIndex + ".png";
+RenderUrlsToFile = function (pages, callbackPerUrl, callbackFinal)
+{
+    console.log("LOGIN: pages: " + pages[0].name);
+
+    var urlIndex = 0,
+        // crypto   = require('crypto'),
+        retrieve, next, getFilename, getMd5Sum, page;
+
+    getMd5Sum = function (string)
+    {
+        // return crypto.createHash('md5').update(string).digest('hex');
+        return string;
     };
-    next = function(status, url, file) {
+
+    getFilename = function (url)
+    {
+        return "./current/" + urlIndex + ".png";
+        // return "./current/" + getMd5Sum(pages[urlIndex].url) + ".png";
+    };
+
+    next = function (status, url, file)
+    {
         page.close();
         callbackPerUrl(status, url, file);
         return retrieve();
     };
-    retrieve = function() {
+
+    retrieve = function ()
+    {
         var url;
-        if (urls.length > 0) {
-            url = urls.shift();
+        if (pages.length > 0)
+        {
+            url = pages.shift();
             urlIndex++;
-            page = webpage.create();
-            page.viewportSize = {
-                width: 800,
-                height: 600
-            };
-            page.settings.userAgent = "Phantom.js bot";
-            return page.open("http://" + url, function(status) {
-                var file;
-                file = getFilename();
-                if (status === "success") {
-                    return window.setTimeout((function() {
-                        page.render(file);
-                        return next(status, url, file);
-                    }), 200);
-                } else {
-                    return next(status, url, file);
-                }
-            });
-        } else {
+            page = createPage();
+
+            console.log('RENDER: trying to render: ' + url.name);
+            try
+            {
+                return page.open(url.url, function (status)
+                {
+                    var file = getFilename(url.url);
+                    console.log('RENDER: got filename ' + file);
+                    if (status === "success")
+                    {
+                        console.log('RENDER: Successfully rendered ' + url.url);
+
+                        return window.setTimeout((function ()
+                        {
+                            page.render(file);
+                            return next(status, url.url, file);
+                        }), 200);
+                    }
+                    else
+                    {
+                        console.log('RENDER: ERROR: ' + url.url);
+                        return next(status, url.url, file);
+                    }
+                });
+            }
+            catch (err)
+            {
+                CATCH_FUNCTION(err);
+            }
+        }
+        else
+        {
+            console.log('RENDER: reached end of pages.length. exit now');
             return callbackFinal();
         }
     };
     return retrieve();
 };
+
+
+if (system.args.length > 1)
+{
+    confFile = Array.prototype.slice.call(system.args, 1);
+}
+else
+{
+    console.log("Usage: phantomjs diff_restricted_pages.js [confFile]");
+    confFile = "./conf.d/default.json";
+}
+
+config = require(confFile);
+
+// Login durchführen (Cookie speichern)
+login(config, function (err, page)
+{
+    if (err)
+    {
+        // Error
+        console.log(err);
+    }
+    else
+    {
+        // Success
+        // Seiten rendern und in einem TEMP-Ordner ablegen
+        console.log('SUCCESS: logged in. Now trying to render the pages');
+        try
+        {
+            RenderUrlsToFile(
+                config.pages,
+                page,
+                function (status, url, file)
+                {
+                    if (status !== "success")
+                    {
+                        return console.log("Unable to render '" + url + "'");
+                    }
+                    else
+                    {
+                        // Bilder mit erwarteten Ergebnissen vergleichen
+                        // Unterschiede sammeln
+                        // Mail vorbereiten
+                        // Mail versenden
+                        return console.log("Rendered '" + url + "' at '" + file + "'");
+                    }
+                }
+                ,
+                function ()
+                {
+                    page.close();
+                    return phantom.exit();
+                }
+            );
+        }
+        catch (err)
+        {
+            CATCH_FUNCTION(err);
+        }
+    }
+});
